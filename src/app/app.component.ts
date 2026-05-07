3616c1a1fe412f47107b8246b88479c2c4b8ebde
kgaktrpg-ctrl <kgaktrpg@gmail.com>
2026-05-07 17:20:41 +0900

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -100,6 +100,7 @@ export class AppComponent implements OnInit, AfterViewInit, OnDestroy {
 
   get otherPeers(): PeerCursor[] { return [PeerCursor.myCursor, ...Network.peers.filter(peer => peer.isOpen).map(peer => PeerCursor.findByPeerId(peer.peerId))].filter(peerCursor => peerCursor); /* ObjectStore.instance.getObjects(PeerCursor); */ }
   get isRoom(): boolean { return Network.peer?.isRoom; }
+  get isHost(): boolean { return (!Network.peer || !Network.peer.isOpen) ? true : (Network.peer as any).isAdmin; }
 
   private static _noticePlayer: AudioPlayer;
   static get noticePlayer(): AudioPlayer {
